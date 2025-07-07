05e14ab47520a5ea94819945e7478e0c9318edc5
aTu44 <arthurhenrybrito@gmail.com>
2025-07-07 16:02:51 -0300

    criando a logica da classe ContasBasicas

--- Calculadora/Models/ContasBasicas.cs
+++ Calculadora/Models/ContasBasicas.cs
@@ -1,19 +1,34 @@
-class ContasBasicas
-{
-    static double Somar(double x, double y)
-    {
-        return (x + y);
+using System;
+
+    public class ContasBasicas {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+
+        public void LerDados() {
+            Console.Write("Digite o primeiro número: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            num2 = double.Parse(Console.ReadLine());
+        }
+
+        public double Somar() {
+            return num1 + num2;
+        }
+
+        public double Subtrair() {
+            return num1 - num2;
+        }
+
+        public double Multiplicar() {
+            return num1 * num2;
+        }
+
+        public double Dividir() {
+            if (num2 == 0) {
+                throw new DivideByZeroException("Divisão por zero não permitida.");
+            }
+            return num1 / num2;
+        }
     }
-        static double Subtrair(double x, double y)
-    {
-        return (x - y);
-    }
-    static double Dividir(double x, double y)
-    {
-        return (x - y);
-    }
-     static double Mutiplicar(double x, double y)
-    {
-        return (x * y);
-    }
-}
+
